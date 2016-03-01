b328bfe3bb8424e3fb13dbddc6635af1b76a7e71
AnatoliyGergiev <gergiev.tolia@yandex.ru>
2016-03-01 23:21:26 +0200

    Correctly working program
    Without good architecture

---
+++ ConsoleApplication1/Client/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Get = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox_Create = new System.Windows.Forms.TextBox();
+            this.button_Kill = new System.Windows.Forms.Button();
+            this.button_Create = new System.Windows.Forms.Button();
+            this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_Get
+            // 
+            this.button_Get.Location = new System.Drawing.Point(7, 75);
+            this.button_Get.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Get.Name = "button_Get";
+            this.button_Get.Size = new System.Drawing.Size(316, 43);
+            this.button_Get.TabIndex = 0;
+            this.button_Get.Text = "Get processes";
+            this.button_Get.UseVisualStyleBackColor = true;
+            this.button_Get.Click += new System.EventHandler(this.button_Get_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(7, 249);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(315, 260);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox_Create
+            // 
+            this.textBox_Create.Location = new System.Drawing.Point(31, 217);
+            this.textBox_Create.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Create.Name = "textBox_Create";
+            this.textBox_Create.Size = new System.Drawing.Size(263, 22);
+            this.textBox_Create.TabIndex = 2;
+            // 
+            // button_Kill
+            // 
+            this.button_Kill.Location = new System.Drawing.Point(7, 121);
+            this.button_Kill.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Kill.Name = "button_Kill";
+            this.button_Kill.Size = new System.Drawing.Size(316, 43);
+            this.button_Kill.TabIndex = 0;
+            this.button_Kill.Text = "Kill process";
+            this.button_Kill.UseVisualStyleBackColor = true;
+            this.button_Kill.Click += new System.EventHandler(this.button_Kill_Click);
+            // 
+            // button_Create
+            // 
+            this.button_Create.Location = new System.Drawing.Point(7, 166);
+            this.button_Create.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Create.Name = "button_Create";
+            this.button_Create.Size = new System.Drawing.Size(316, 43);
+            this.button_Create.TabIndex = 3;
+            this.button_Create.Text = "Create process";
+            this.button_Create.UseVisualStyleBackColor = true;
+            this.button_Create.Click += new System.EventHandler(this.button_Create_Click);
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Location = new System.Drawing.Point(201, 36);
+            this.textBox_IP.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.Size = new System.Drawing.Size(97, 22);
+            this.textBox_IP.TabIndex = 2;
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(16, 18);
+            this.button_Connect.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(164, 43);
+            this.button_Connect.TabIndex = 0;
+            this.button_Connect.Text = "Connect to server";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP-address of server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 516);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Create);
+            this.Controls.Add(this.textBox_IP);
+            this.Controls.Add(this.textBox_Create);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button_Kill);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.button_Get);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Get;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox_Create;
+        private System.Windows.Forms.Button button_Kill;
+        private System.Windows.Forms.Button button_Create;
+        private System.Windows.Forms.TextBox textBox_IP;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server srv = new Server();
+            srv.Accept();
+            Console.Read();
+        }
+        class Server
+        {
+            public Server()
+            {
+                uiContext = SynchronizationContext.Current;
+            }
+            public SynchronizationContext uiContext;
+            private async void Exchange(Socket handler, List<string> theMessage)
+            {
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        byte[] msg;
+                        int bytesSent;
+                        foreach (string s in theMessage)
+                        {
+                            msg = Encoding.Default.GetBytes(s); // конвертируем строку, содержащую сообщение, в массив байтов
+                            bytesSent = handler.Send(msg); // отправляем серверу сообщение через сокет
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("\n"+ex.Message+"\n");
+                    }
+                });
+            }
+            private async void Receive(Socket handler)
+            {
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        string client = null;
+                        string data = null;
+                        byte[] bytes = new byte[1024];
+                        // Получим от клиента DNS-имя хоста.
+                        // Метод Receive получает данные от сокета и заполняет массив байтов, переданный в качестве аргумента
+                        int bytesRec = handler.Receive(bytes); // Возвращает фактически считанное число байтов
+                        client = Encoding.Default.GetString(bytes, 0, bytesRec); // конвертируем массив байтов в строку
+                        client += "(" + handler.RemoteEndPoint.ToString() + ")";
+                        while (true)
+                        {
+                            bytesRec = handler.Receive(bytes); // принимаем данные, переданные клиентом. Если данных нет, поток блокируется
+                            if (bytesRec == 0)
+                            {
+                                handler.Shutdown(SocketShutdown.Both); // Блокируем передачу и получение данных для объекта Socket.
+                                handler.Close(); // закрываем сокет
+                                return;
+                            }
+                            data = Encoding.Default.GetString(bytes, 0, bytesRec); // конвертируем массив байтов в строку
+                            if (data.Contains("Get processes"))
+                            {
+                                List<string> processNames = new List<string>();
+                                foreach (Process process in Process.GetProcesses())
+                                    processNames.Add(process.ProcessName+"~");
+                                Exchange(handler, processNames);
+                            }
+                            else if (data.Contains("Create process"))
+                            {
+                                data = data.Replace("Create process", null);
+                                Process.Start(data);
+                            }
+                            else if (data.Contains("Kill process"))
+                            {
+                                data = data.Replace("Kill process", null);
+                                Process[] lp = Process.GetProcessesByName(data);
+                                lp[0].Kill();
+                            }
+                            else if (data == "Disconnect") // если клиент отправил эту команду, то заканчиваем обработку сообщений
+                                break;
+                        }
+                        string theReply = "Я завершаю обработку сообщений";
+                        byte[] msg = Encoding.Default.GetBytes(theReply); // конвертируем строку в массив байтов
+                        handler.Send(msg); // отправляем клиенту сообщение
+                        handler.Shutdown(SocketShutdown.Both); // Блокируем передачу и получение данных для объекта Socket.
+                        handler.Close(); // закрываем сокет
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("\nСервер: " + ex.Message+"\n");
+                        //handler.Shutdown(SocketShutdown.Both); // Блокируем передачу и получение данных для объекта Socket.
+                        //handler.Close(); // закрываем сокет
+                    }
+                });
+            }
+
+            //  ожидать запросы на соединение будем в отдельном потоке
+            public async void Accept()
+            {
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        // установим для сокета адрес локальной конечной точки
+                        // уникальный адрес для обслуживания TCP/IP определяется комбинацией IP-адреса хоста с номером порта обслуживания
+                        IPEndPoint ipEndPoint = new IPEndPoint(
+                       IPAddress.Any /* Предоставляет IP-адрес, указывающий, что сервер должен контролировать действия клиентов на всех сетевых интерфейсах.*/,
+                       49152 /* порт */);
+
+                        // создаем потоковый сокет
+                        Socket sListener = new Socket(AddressFamily.InterNetwork /*схема адресации*/, SocketType.Stream /*тип сокета*/, ProtocolType.Tcp /*протокол*/ );
+                        /* Значение InterNetwork указывает на то, что при подключении объекта Socket к конечной точке предполагается использование IPv4-адреса.
+                           SocketType.Stream поддерживает надежные двусторонние байтовые потоки в режиме с установлением подключения, без дублирования данных и 
+                           без сохранения границ данных. Объект Socket этого типа взаимодействует с одним узлом и требует предварительного установления подключения 
+                           к удаленному узлу перед началом обмена данными. Тип Stream использует протокол Tcp и схему адресации AddressFamily.
+                         */
+
+                        // Чтобы сокет клиента мог идентифицировать потоковый сокет TCP, сервер должен дать своему сокету имя
+                        sListener.Bind(ipEndPoint); // Свяжем объект Socket с локальной конечной точкой.
+
+                        // Установим объект Socket в состояние прослушивания.
+                        sListener.Listen(10 /* Максимальная длина очереди ожидающих подключений.*/ );
+                        while (true)
+                        {
+                            /* Блокируется поток до поступления от клиента запроса на соединение. При этом устанавливается связь имен клиента и сервера. 
+                               Метод Accept извлекает из очереди ожидающих запросов первый запрос на соединение и создает для его обработки новый сокет.
+                               Хотя новый сокет создан, первоначальный сокет продолжает слушать и может использоваться с многопоточной обработкой для 
+                               приема нескольких запросов на соединение от клиентов. Сервер не должен закрывать слушающий сокет, который продолжает работать
+                               наряду с сокетами, созданными методом Accept для обработки входящих запросов клиентов.
+                             */
+                            Socket handler = sListener.Accept();
+                            Console.WriteLine("Подключился клиент {0}\n", handler.RemoteEndPoint);
+                            // обслуживание текущего запроса будем выполнять в отдельной асинхронной задаче
+                            Receive(handler);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Сервер: " + ex.Message);
+                    }
+                });
+            }
+        }
+    }
+}
